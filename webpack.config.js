cf3968256d5de198e5d2114ac3100c80869f53a3
USUARIO <78711175+pgap22@users.noreply.github.com>
2023-08-20 21:55:48 -0600

    deploy 7

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
-const { readdirSync } = require("fs");
-const { resolve } = require("path");
-
+import { readdirSync } from "fs";
+import { resolve } from "path";
+import * as url from 'url';
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const nodeModules = {};
 
 readdirSync(resolve(__dirname, 'node_modules'))
     .filter(x => ['.bin'].indexOf(x) === -1)
     .forEach(mod => { nodeModules[mod] = "commonjs "+mod });
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-      path: resolve(__dirname),
-      filename: 'api-build.cjs',
-    },
-    externals: nodeModules,
-    target: 'node',
+const entry = './src/index.js';
+const output = {
+  path: resolve(__dirname),
+  filename: 'api-build.cjs',
 };
+const externals = nodeModules;
+const target = 'node';
+
+export default {
+  entry,
+  output,
+  externals,
+  target
+}
